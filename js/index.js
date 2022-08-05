aa0fb2ce30ee7173c73ed4309181a22cba5013bf
Paul Nizet <polonizet@gmail.com>
2022-08-05 15:34:40 +0200

    Draw road image on click 'Start button' after image onload event

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
+    console.log("clicked 1")
   };
 
-  function startGame() {}
-};
+  function startGame() {
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d")
+
+    // Initialize and draw the road
+
+    const img = new Image();
+    img.src = './images/road.png'
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0)
+    }
+
+    // ctx.fillStyle = 'purple';
+    // ctx.fillRect(40, 40, 100, 100);
+  }
+}
